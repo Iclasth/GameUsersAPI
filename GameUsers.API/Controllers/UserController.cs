3884a8ae90960e3cd1adb2c607f1240cab872697
iclasTH <iclasinprogramacao@gmail.com>
2025-10-27 22:49:37 -0300

    Criando os end-ponts fundamentais no controller

---
+++ GameUsers.API/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameUsers.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        [HttpPost("register")]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+        [HttpGet("me")]
+        public IActionResult GetUsers()
+        {
+            return Ok();
+        }
+        
+    }
+}
