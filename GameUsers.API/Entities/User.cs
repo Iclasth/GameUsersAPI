532462367ae24f9091eb99bc393fa3e14fdb50a6
iclasTH <iclasinprogramacao@gmail.com>
2025-12-12 17:21:50 -0300

    feat(dbContext, Entities): Configurando a conexão com o banco de dados através do Entity Framework e Criando a entidade básica.
    É de suma importancia que para criar um bom sistema de Login, é indispensável registrar os usúarios no banco para que eles tenham acesso a suas contas.

---
+++ GameUsers.API/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace GameUsers.API.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string DisplayName { get; set; } = string.Empty;
+
+        public string Class { get; set; } = string.Empty;
+        public int Level { get; set; }
+
+
+    }
+}
---
+++ GameUsers.API/Infraestructure/GameUsersDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GameUsers.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameUsers.API.Infraestructure
+{
+    public class GameUsersDbContext : DbContext
+    {
+        public DbSet<User> Users {get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Banco de Dados\\Entity Framework\\GameUsersAPIEF\\gameusers.db");
+        }
+    }
+}
