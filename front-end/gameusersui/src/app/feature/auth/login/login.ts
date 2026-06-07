bcc922df469558a0d1e048e2749afad891acef91
iclasTH <iclasinprogramacao@gmail.com>
2026-06-07 20:01:33 -0300

    feat(FormField): Criando o componente form-field e Implementando o ReactiveForms
    Primeiro, adicionei o Reactive form no meu programa para que ele fique responsável pela validação e modelagem de dados em tempo real na aplicação. Logo após isso, criei um componente chamado form-field para representar uma estilização global para os elementos de formulário. Assim, facilitando a adição de novos campos.

--- front-end/gameusersui/src/app/feature/auth/login/login.ts
+++ front-end/gameusersui/src/app/feature/auth/login/login.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
 import { AuthCard } from '../../../shared/components/auth-card/auth-card';
+import { FormField } from '../../../shared/components/form-field/form-field';
 import { RouterLink } from "@angular/router";
+import { ReactiveFormsModule, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [AuthCard, RouterLink],
+  imports: [AuthCard, RouterLink, FormField, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(25)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(100)])
+  });
+
+  onsubmit() 
+  {
+    if (this.loginForm.valid) {
+      console.log('Dados do formulário: ', this.loginForm.value);
+    }
+    else {
+      console.log('Dados inválidos! ')
+    }
+  } 
+}
+
+
+
