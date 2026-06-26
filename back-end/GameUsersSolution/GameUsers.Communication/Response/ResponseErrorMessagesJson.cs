03709987c84bd719eebd5e2b57b71fc0948ff18d
iclasTH <iclasinprogramacao@gmail.com>
2026-06-26 10:56:36 -0300

    feat(Exception Filter): Implementando Exceptions personalizadas no meu programa
    Foram implementadas exceptions para diferentes casos, como do HTTP response code "401" (no content)

---
+++ back-end/GameUsersSolution/GameUsers.Communication/Response/ResponseErrorMessagesJson.cs
@@ -0,0 +1,17 @@
+﻿namespace GameUsers.Communication.Response
+{
+    public class ResponseErrorMessagesJson
+    {
+        public List<string> ErrorsMessages { get; private set; }
+
+        public ResponseErrorMessagesJson(string message)
+        {
+            ErrorsMessages = [message];
+        }
+
+        public ResponseErrorMessagesJson(List<string> messages) 
+        {
+            ErrorsMessages = messages;
+        }
+    }
+}
---
+++ back-end/GameUsersSolution/GameUsers.Exceptions/ExceptionsBase/NotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace GameUsers.Exceptions.ExceptionsBase
+{
+    public class NotFoundException : GameUsersExceptions
+    {
+        public NotFoundException(string errorMessage) : base(errorMessage)
+        {
+        }
+
+        public override List<string> GetErrors() => [Message];
+        
+
+        public override HttpStatusCode GetHttpStatusCode() => HttpStatusCode.NotFound;
+       
+    }
+}
